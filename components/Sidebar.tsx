90bfb4f177d20353a730c10feaae153a178964d3
Anirudh R <120504102+anirudhwillcode@users.noreply.github.com>
2024-06-08 13:30:00 +0530

    added home,previous,recordings,upcomming

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,19 +10,20 @@ const Sidebar = () => {
   const pathname = usePathname();
 
   return (
-    <section className="sticky left-0 top-20 flex h-screen w-fit flex-col  justify-between  bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
-      <div className="flex flex-1 flex-col gap-6">
+    <aside className="sticky left-0 top-0 h-screen w-[264px] bg-dark-1 p-6 pt-48 text-white max-sm:hidden">
+      <div className="flex flex-col gap-6 overflow-y-auto">
         {sidebarLinks.map((item) => {
           const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
-          
+
           return (
             <Link
               href={item.route}
               key={item.label}
               className={cn(
-                'flex gap-4 items-center p-4 rounded-lg justify-start',
+                'flex gap-4 items-center p-4 rounded-lg justify-start transition-colors duration-300',
                 {
                   'bg-blue-1': isActive,
+                  'hover:bg-gray-700': !isActive
                 }
               )}
             >
@@ -32,14 +33,14 @@ const Sidebar = () => {
                 width={24}
                 height={24}
               />
-              <p className="text-lg font-semibold max-lg:hidden">
+              <p className="text-lg font-semibold">
                 {item.label}
               </p>
             </Link>
           );
         })}
       </div>
-    </section>
+    </aside>
   );
 };
 
